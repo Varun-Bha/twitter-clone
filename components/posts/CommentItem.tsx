014a8d4f7e4d925bbcb77f6b675fac60e2285703
Varun-Bha <varunbha012@gmail.com>
2025-07-19 17:50:40 +0530

    fixed bugs

--- components/posts/CommentItem.tsx
+++ components/posts/CommentItem.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import { useCallback, useMemo } from 'react';
+import { useCallback } from 'react';
 import { formatDistanceToNowStrict } from 'date-fns';
 
 import Avatar from '../Avatar';
